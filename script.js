21e8ef9df05bde5378a31603451fd3f57f372643
misojo <misojo@outlook.com>
2023-06-25 20:01:28 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,127 @@
+//STEP 01
+// camelCase
+//let someMonth;
+//function theMonth();
+//let currentMonth;
+//let summerMonth;
+//let myLibraryFunction;
+
+//STEP 02
+//numeric literal:
+//var eggs = 12;
+//string literal:
+//var compliment = 'You are looking lovely this morning!';
+//boolean literal:
+//var victory = false;
+//null literal:
+//var abc = null;
+
+//STEP 03
+//2 Complex variable expressions
+//let express1 = 2 + (100/48)-2;
+//let express2 = 3 * (5/4) + 6;
+
+//STEP 04
+//camelCase & Hungarian 
+//let strFirstName;  //First Name with strings
+//let strLastName;   //Last Name with strings
+//let strAddress;    //Address with strings
+//let strCity;       //City with strings
+//let strState;      //State with strings
+//let iZipCode;      //Zip Code with integers
+//let iAge;          //Your Age with integers
+//let strRefSrc;     //Referral Source
+//let bContact;      //May We Contact You with a boolean
+
+//STEP 05
+//Declaring then Assigning 3 of the 9 variables
+//let strFirstName;
+//strFirstName = "Leslie";
+
+//let strLastName = "Jo";
+
+//let bContact = true;
+
+//STEP 06
+//Create a variable. Display a number and a string in the console window. 
+//let eggs = 20;
+//console.log(" The number of eggs in the refrigerator is " + eggs);
+
+//STEP 07
+//Review later
+//var correctAnswer = "true";
+//var wrongAnswer = "false";
+
+//STEP 08
+//let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I am not sure about the former."';
+//window.console.log(someString);
+//The "I'm" in the original quote disrupts the string so either swap out the '' quotation marks for "" or change I'm to I am. 
+
+//STEP 09
+//null value
+//var a = null;
+//console.log(a);
+
+//undefined value
+//var b;
+//console.log(b);
+
+//STEP 10
+//Unary operator: Undefined
+//var x;  // x is undefined
+//var y = +x;
+//console.log(y); //still undefined
+
+//+true; //Boolean
+//+null; //Yields 0
+
+//STEP 11
+//alert("Hello" + ' Leslie Jo, ' + "Welcome to Javascript Class");
+
+//STEP 12
+//let nameFull = " Leslie Jo! ";
+//alert("Hello" + nameFull + "Welcome to Javascript Class"); 
+
+//STEP 13
+//let course = "Javascript";
+//alert("Hello" + nameFull + course);
+//STEP 14
+//alert("Hello" + nameFull + "\n" + course);
+
+//STEP 15
+//alert("Please enter your name:");
+//prompt();
+//nameFull = prompt;
+
+//STEP 16
+//alert("Please enter your class: ")
+//prompt();
+//course = prompt;
+
+//STEP 17
+//var x = 10;
+//var y =20;
+//console.log( x + y);
+
+//STEP 18
+//let x = 20;
+// x +=20;
+// console.log(x);
+
+//STEP 19
+//let x = 20;
+//x *=5;
+//console.log(x);
+
+//STEP 20
+//let x = 20/3;
+//x /=1;
+//console.log(x);
+//STEP 21
+// let x = 5;
+// let y = 5;
+//console.log(x==y && y==5);
+
+//STEP 22
+//let x = 2;
+//console.log(x !=2)
